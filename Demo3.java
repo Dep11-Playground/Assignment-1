46d3154b7328e34f3b34e6c3cd92115eeedd0b0c
sasiri-jambugaswatta <jambugaswattasasiri@gmail.com>
2023-07-31 00:26:31 +0530

    Complete Task 3

--- Demo3.java
+++ Demo3.java
@@ -1,5 +1,9 @@
 public class Demo3 {
     public static void main(String[] args) {
-        
+        int country = 123;
+        int middle = 456;
+        int end = 7890;
+
+        System.out.printf("\033[32m(%d)\033[0m %d-%d\n",country,middle,end);
     }
 }
