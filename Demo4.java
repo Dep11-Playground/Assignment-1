02545e868ffb592e5257691b683b168504d9bb82
sasiri-jambugaswatta <jambugaswattasasiri@gmail.com>
2023-07-31 00:46:30 +0530

    Complete Task 4

--- Demo4.java
+++ Demo4.java
@@ -1,5 +1,14 @@
 public class Demo4 {
     public static void main(String[] args) {
-        
+        int day = 28;
+        int month = 12;
+        int year = 2023;
+        int hour = 23;
+        int minute = 59;
+        int second = 59;
+
+        String reset = "\033[0m";
+
+        System.out.printf("\033[32m%d%2s/\033[33m%d%2$s/\033[34m%d%2$s \033[35m%d%2$s:\033[36m%d%2$s:\033[39m%d\n",month,reset,day,year,hour,minute,second);
     }
 }
