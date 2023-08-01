222946bf61839600523e60212eed0b97e6fa79a1
sasiri-jambugaswatta <jambugaswattasasiri@gmail.com>
2023-08-01 10:45:08 +0530

    Complete Task 6-B

---
+++ Demo6TaskC.java
@@ -0,0 +1,7 @@
+public class Demo6TaskC{
+    public static void main(String[] args) {
+        String bar = "\033[38;5;179m|\033[0m";
+
+        System.out.printf("\033[38;5;179m+%s+%s+%s+\033[0m\n","-----------------","-------------","------------");
+    }
+}
